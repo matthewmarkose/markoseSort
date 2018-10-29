69c5d2a2a091f8484fbc5f9bae682ce62c92f9aa
mmarkose <mmarkose@iastate.edu>
2018-10-29 10:56:47 -0500

    visited feature

--- begin/First.java
+++ begin/First.java
@@ -1,13 +1,17 @@
 package begin;
+
+import java.util.HashSet;
+
 /*
  * 10/28/18 Matthew Markose
  */
 public class First {
 	static int[] arr= {63,65,38,59,10,62,9,66,42,74,84,11,79,60,74,51,74,41,36,72,30,40,15,24,85,56,86,76,84,5};
+	static HashSet<Integer> visited=new HashSet<Integer>();
 	public static void main(String[] args){
 		
 		//Sort Array
-		markoseSort(0, arr.length-1);
+		markoseSortNotVisited(0, arr.length-1);
 		
 		//Print Array
 		for(int i:arr){
@@ -15,21 +19,41 @@ public class First {
 		}
 	}
 
-	private static void markoseSort(int i, int j) {
+	private static void markoseSortNotVisited(int right, int left) {
 		
-		//if comparing itself
-		if(i!=j){
+		//if comparing itself or already visited
+		if(right!=left && !visited.contains(right*arr.length+left)){
+			
+			// mark comparison as visited 
+			visited.add(right*arr.length+left);
 			
 			// switch if left>right
-			if(arr[i]>arr[j]){
-				int temp=arr[i];
-				arr[i]=arr[j];
-				arr[j]=temp;
+			if(arr[right]>arr[left]){
+				int temp=arr[right];
+				arr[right]=arr[left];
+				arr[left]=temp;
 			}
 			
 			//compare other indexes
-			markoseSort(i+1, j);
-			markoseSort(i, j-1);
+			markoseSortNotVisited(right+1, left);
+			markoseSortNotVisited(right, left-1);
+		}
+	}
+	
+	private static void markoseSortRepeatVisited(int right, int left) {
+		//if comparing itself
+		if(right!=left){
+			
+			// switch if left>right
+			if(arr[right]>arr[left]){
+				int temp=arr[right];
+				arr[right]=arr[left];
+				arr[left]=temp;
+			}
+			
+			//compare other indexes
+			markoseSortRepeatVisited(right+1, left);
+			markoseSortRepeatVisited(right, left-1);
 		}
 	}
 }
